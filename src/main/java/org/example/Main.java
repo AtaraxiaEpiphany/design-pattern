0eb37dfb6d9075aa3c19dd022583e1c78bb7f016
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-07-23 10:49:15 +0800

    Init.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Hannibal
+ * @date 22/07/2023 16:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
