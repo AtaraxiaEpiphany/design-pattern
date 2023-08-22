9809e9109fc4dc290f8d8411c4b1fc125382b91f
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-08-22 19:46:08 +0800

    Init Project.

---
+++ security-service-contract/src/main/java/com/demo/security/Main.java
@@ -0,0 +1,14 @@
+package com.demo.security;
+
+/**
+ * @Title: ${NAME} ${MEANING}
+ * @Author Hannibal
+ * @Package com.demo.security
+ * @Date 22/08/2023 19:30
+ * @Description: TODO ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
